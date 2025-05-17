a9948eec160e9340209a0d5a3603bef9a0f6d02d
Torch <rustruedetective@gmail.com>
2025-05-17 16:16:14 +0500

    changes and experiments

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from llms.llmopenai import LLMOPENAI
+from model.gen.llm._openai import gloa
+import time
 
-llm = LLMOPENAI(prty="What is the largest export of {}?")
-llm.run(["pakistan"]).show_responses()
+llm1 = gloa(name="model 1")
+llm2 = gloa(name="model 2")
 
-llm.set_prty("What is capital of {}?").run(["russia"]).show_responses()
+a = llm1.set("Ask something you want from another llm model.{}").run([""]).get()
+time.sleep(1)
+b = llm2.set("Ask something you want from another llm model who has said: {}").run([a]).get()
+time.sleep(1)
+
+for i in range(10):
+    a = llm1.set("Model 2 has asked: {}").run([b]).get()
+    print("Model 1: ", a)
+    print("---")
+    time.sleep(1)
+    b = llm2.set("Model 1 has asked: {}").run([a]).get()
+    print("Model 2: ", b)
+    print("---")
+    time.sleep(1)
